c3eb669257c60fa6e5f42a35a5652c686f72fa9a
raeunlee <raeun19@naver.com>
2024-09-02 20:07:17 +0900

    Create 두 방향 탈출 가능 여부 판별하기.py

---
+++ 2024.09/codetree/알고리즘입문/DFS/두 방향 탈출 가능 여부 판별하기.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+
+l = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    l.append(row)
+
+visited = [[0 for _ in range(m)] for _ in range(n)]
+
+dir = [(0, 1), (1, 0)]
+
+def dfs(x, y):
+    for d in dir:
+        nx = x + d[0]
+        ny = y + d[1]
+        if 0 <= nx < n and 0 <= ny < m:  # 인덱스가 유효한지 먼저 확인
+            if l[nx][ny] == 1 and not visited[nx][ny]:  # 이후에 방문 및 값 체크
+                visited[nx][ny] = 1
+                dfs(nx, ny)
+
+visited[0][0] = 1
+dfs(0, 0)
+print(visited[n - 1][m - 1])
